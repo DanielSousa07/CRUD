be26319609f00034529cadb5e990db345d5da6c5
Joseni <josenidaniel88@gmail.com>
2025-09-20 15:17:34 -0300

    Tudo finalizado, agora só treinar mais

--- src/controllers/ClientController.js
+++ src/controllers/ClientController.js
@@ -12,7 +12,21 @@ module.exports = {
         const client = await Client.create({email, nome, rua, numero, bairro, cep, cidade, uf})
         return res.json(client)
     },
-    async index(req, res) {},
-    async update(req, res) {},
-    async delete(req, res) {}
+    async index(req, res) {
+        const clients = await Client.find()
+        return res.json(clients)
+    },
+    async update(req, res) {
+        const {id} = req.params 
+        const {email, nome, rua, numero, bairro, cep, cidade, uf} = req.body
+
+        const client = await Client.findByIdAndUpdate(id, {email, nome, rua, numero, bairro, cep, cidade, uf}, {new: true})
+        return res.json(client)
+    },
+    async delete(req, res) {
+        const {id} = req.params
+
+        await Client.findByIdAndDelete(id)
+        return res.json({sucesso: 'Cliente removido com sucesso'})
+    }
 }
--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,9 @@ const clientController = require('./controllers/ClientController')
 const routes = express.Router();
 
 routes.post('/insert', clientController.store)
+routes.get('/list', clientController.index)
+routes.put('/update/:id', clientController.update)
+routes.delete('/delete/:id', clientController.delete)
 
 
 module.exports = routes
